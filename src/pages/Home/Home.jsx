b000b7673df9a9e8e1f8bfdbea7c6dcf302d3d0f
Danish <dnazir343@gmail.com>
2024-04-07 21:20:18 +0500

    little bug fix in Services Page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Header from '../../components/Header/Header'
 import { About, Experience, Projects, Skills, Services } from "../index"
 
 
+
 const Home = () => {
   return (
     <div>
@@ -25,6 +26,7 @@ const Home = () => {
       <div className="mt-[100px]">
         <Experience />
       </div>
+
 
     </div>
   )
